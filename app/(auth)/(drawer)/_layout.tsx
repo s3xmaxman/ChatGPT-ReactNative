7534f60b56ff2d4b14ca43416fa41a0cfea8aa3d
s3xmaxman <badside3919@gmail.com>
2024-07-08 11:24:23 +0900

    build(deps): update _layout.tsx, _layout.tsx, package-lock.json and package.json

--- app/(auth)/(drawer)/_layout.tsx
+++ app/(auth)/(drawer)/_layout.tsx
@@ -1,12 +1,81 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
+import { Drawer } from "expo-router/drawer";
+import Colors from "@/constants/Colors";
 
-const _layout = () => {
+const Layout = () => {
   return (
-    <View>
-      <Text>_layout</Text>
-    </View>
+    <Drawer>
+      <Drawer.Screen
+        name="(chat)/new"
+        options={{
+          title: "New Chat",
+          drawerIcon: () => (
+            <View>
+              <Text>New Chat</Text>
+            </View>
+          ),
+        }}
+      />
+    </Drawer>
   );
 };
 
-export default _layout;
+const styles = StyleSheet.create({
+  searchSection: {
+    marginHorizontal: 16,
+    borderRadius: 10,
+    height: 34,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.input,
+  },
+  searchIcon: {
+    padding: 6,
+  },
+  input: {
+    flex: 1,
+    paddingTop: 8,
+    paddingRight: 8,
+    paddingBottom: 8,
+    paddingLeft: 0,
+    alignItems: "center",
+    color: "#424242",
+  },
+  footer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 12,
+  },
+  roundImage: {
+    width: 30,
+    height: 30,
+  },
+  avatar: {
+    width: 40,
+    height: 40,
+    borderRadius: 10,
+  },
+  userName: {
+    fontSize: 16,
+    fontWeight: "600",
+    flex: 1,
+  },
+  item: {
+    borderRadius: 15,
+    overflow: "hidden",
+  },
+  btnImage: {
+    margin: 6,
+    width: 16,
+    height: 16,
+  },
+  dallEImage: {
+    width: 28,
+    height: 28,
+    resizeMode: "cover",
+  },
+});
+
+export default Layout;
--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -3,7 +3,11 @@ import React from "react";
 import { Stack } from "expo-router";
 
 const Layout = () => {
-  return <Stack></Stack>;
+  return (
+    <Stack>
+      <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
+    </Stack>
+  );
 };
 
 export default Layout;

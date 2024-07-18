ea705efc1c9155332d81e7eb76359ed8cb1bcf7b
s3xmaxman <badside3919@gmail.com>
2024-07-18 14:33:27 +0900

    update new.tsx and HeaderDropDown.tsx

--- app/(auth)/(drawer)/(chat)/new.tsx
+++ app/(auth)/(drawer)/(chat)/new.tsx
@@ -1,12 +1,32 @@
 import { View, Text, Button } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { useAuth } from "@clerk/clerk-expo";
+import { Stack } from "expo-router";
 import { defaultStyles } from "@/constants/Styles";
+import HeaderDropDown from "@/components/HeaderDropDown";
 
 const Page = () => {
   const { signOut } = useAuth();
+  const [gptVersion, setGptVersion] = useState("3.5");
   return (
     <View style={defaultStyles.pageContainer}>
+      <Stack.Screen
+        options={{
+          headerTitle: () => (
+            <HeaderDropDown
+              title="ChatGPT"
+              onSelect={(key) => {
+                setGptVersion(key);
+              }}
+              selected={gptVersion}
+              items={[
+                { key: "3.5", title: "GPT-3.5", icon: "bolt" },
+                { key: "4", title: "GPT-4", icon: "sparkles" },
+              ]}
+            />
+          ),
+        }}
+      />
       <Button title="Sign Out" onPress={() => signOut()} />
     </View>
   );
--- components/HeaderDropDown.tsx
+++ components/HeaderDropDown.tsx
@@ -1,11 +1,43 @@
 import { View, Text } from "react-native";
 import React from "react";
+import * as DropdownMenu from "zeego/dropdown-menu";
+import { Ionicons } from "@expo/vector-icons";
 
-const HeaderDropDown = () => {
+export type HeaderDropDownProps = {
+  title: string;
+  selected?: string;
+  onSelect: (key: string) => void;
+  items: Array<{ key: string; title: string; icon: string }>;
+};
+
+const HeaderDropDown = ({
+  title,
+  selected,
+  onSelect,
+  items,
+}: HeaderDropDownProps) => {
   return (
-    <View>
-      <Text>HeaderDropDown</Text>
-    </View>
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+        <View>
+          <Text style={{ fontWeight: "500", fontSize: 16 }}>{title}</Text>
+        </View>
+      </DropdownMenu.Trigger>
+      {/* @ts-ignore */}
+      <DropdownMenu.Content>
+        {items.map((item) => (
+          <DropdownMenu.Item key={item.key} onSelect={() => onSelect(item.key)}>
+            <DropdownMenu.ItemTitle>{item.title}</DropdownMenu.ItemTitle>
+            <DropdownMenu.ItemIcon
+              ios={{
+                name: item.icon,
+                pointSize: 18,
+              }}
+            />
+          </DropdownMenu.Item>
+        ))}
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
   );
 };
 

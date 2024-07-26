b6801ac3643cd7d1a1bf9953958b3577b3de866f
s3xmaxman <badside3919@gmail.com>
2024-07-26 15:19:24 +0900

    update Image.ts

--- utils/Image.ts
+++ utils/Image.ts
@@ -9,7 +9,64 @@ export const downloadAndSaveImage = async (imageUrl: string) => {
 
   try {
     const res = await FileSystem.downloadAsync(fileUri, imageUrl);
+    return saveFile(res.uri);
   } catch (error) {
     console.log("downloadAndSaveImage error", error);
   }
+};
+
+const saveFile = async (fileUri: string) => {
+  const { status } = await MediaLibrary.requestPermissionsAsync();
+  if (status === "granted") {
+    try {
+      const asset = await MediaLibrary.createAssetAsync(fileUri);
+      const album = await MediaLibrary.getAlbumAsync("Download");
+
+      if (album === null) {
+        const result = await MediaLibrary.createAlbumAsync(
+          "Download",
+          asset,
+          false
+        );
+
+        if (result) {
+          Alert.alert("Image saved to Photos");
+        }
+      } else {
+        const result = await MediaLibrary.addAssetsToAlbumAsync(
+          [asset],
+          album,
+          false
+        );
+
+        if (result) {
+          Alert.alert("Image saved to Photos");
+        }
+      }
+    } catch (error) {
+      console.log("saveFile error", error);
+    }
+  } else if (status === "denied") {
+    Alert.alert("please allow permissions to download");
+  }
+};
+
+export const copyImageToClipboard = async (imageUrl: string) => {
+  let fileUriCopy =
+    FileSystem.documentDirectory + `${new Date().getTime()}.jpg`;
+
+  try {
+    const res = await FileSystem.downloadAsync(imageUrl, fileUriCopy);
+    const base64 = await FileSystem.readAsStringAsync(res.uri, {
+      encoding: FileSystem.EncodingType.Base64,
+    });
+
+    await Clipboard.setStringAsync(base64);
+  } catch (error) {
+    console.log("copyImageToClipboard error", error);
+  }
+};
+
+export const shareImage = async (imageUrl: string) => {
+  Sharing.shareAsync(imageUrl);
 };

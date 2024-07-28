0ac09d4b86795a74dd6fba85480a07aba60ff67c
s3xmaxman <badside3919@gmail.com>
2024-07-29 02:19:56 +0900

    update [url].tsx

--- app/(auth)/(modal)/[url].tsx
+++ app/(auth)/(modal)/[url].tsx
@@ -1,7 +1,9 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { useLocalSearchParams } from "expo-router";
 
 const page = () => {
+  const { url, params } = useLocalSearchParams();
   return (
     <View>
       <Text>page</Text>

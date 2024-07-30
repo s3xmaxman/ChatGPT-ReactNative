037e07820b10886757c2181b37316e0c7d97cc08
s3xmaxman <badside3919@gmail.com>
2024-07-30 19:30:22 +0900

    build(deps): update explore.tsx, ChatMessage.tsx, package-lock.json and package.json

---
+++ constants/Apps.ts
@@ -0,0 +1,42 @@
+export const apps = [
+  {
+    title: "インスタントウェブサイト [マルチページ]",
+    description:
+      "スタートアップや小規模ビジネスのニーズに応える機能的なマルチページウェブサイトを生成します。新機能で継続的に更新されます。",
+    author: "マックス & キリル・ドゥボビツキー著",
+    image:
+      "https://files.oaiusercontent.com/file-9P4NhIxlr14rKlHEM41VVbxS?se=2124-03-21T08%3A51%3A45Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D1209600%2C%20immutable&rscd=attachment%3B%20filename%3Dsdfgasdfx.jpg&sig=8xHssdF2qgY0qpyaUNRn4My5tJwh9iYMn1Lg53H9Z1c%3D",
+  },
+  {
+    title: "ダイアグラム & データ",
+    description:
+      "複雑なデータをダイアグラムやチャートを通じて研究、分析、視覚化するのに役立ちます。コーダーやビジネスアナリストにとって便利です。",
+    author: "マックス & キリル・ドゥボビツキー著",
+    image:
+      "https://files.oaiusercontent.com/file-teufH6uVdqxmxHjEUIQjD8ur?se=2124-03-24T19%3A02%3A04Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D1209600%2C%20immutable&rscd=attachment%3B%20filename%3Dvar6.jpg&sig=wn6KyKdgbqJ1gGkHltYV8cl3/ZwLZmgO039GkueA8Z8%3D",
+  },
+  {
+    title: "ChatPRD",
+    description:
+      "オンデマンドのチーフプロダクトオフィサーとして機能し、プロダクト要件文書を強化し、プロダクトマネージャーやエンジニアへのコーチングを提供します。",
+    author: "クレア・V・ローウェス著",
+    image:
+      "https://files.oaiusercontent.com/file-qeVpUG3AJT0FINT4eZ6Gbt2q?se=2123-10-17T23%3A41%3A20Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3Dcvolawless_illustration_of_a_female_ceo_at_a_laptop_lo-fi_asthe_c60ce7fb-5902-474c-aa85-54c7469aa089.png&sig=eHU4/LmvHg96KaqivlhaLufaIleMC1wm3pE0kMQF1AA%3D",
+  },
+  {
+    title: "音楽教師",
+    description:
+      "音楽理論、スケール、制作などに特化しており、カバーアートのための画像生成機能も含まれています。",
+    author: "gryphonedm.com著",
+    image:
+      "https://files.oaiusercontent.com/file-gLZOuk6mmgg4vsCuhWxgQ2Cm?se=2123-12-13T21%3A57%3A04Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D1209600%2C%20immutable&rscd=attachment%3B%20filename%3D3e763913-0301-49ec-b2f0-c9ad832df862.png&sig=cCp02Ji5dcCxt0UPu92vrFgmZ8p1jwkqUYlYbR4IfoY%3D",
+  },
+  {
+    title: "UXデザインメンター",
+    description:
+      "UXまたはプロダクトデザインに関する具体的なフィードバックを提供し、デザインプロセスを向上させます。",
+    author: "コミュニティビルダー著",
+    image:
+      "https://files.oaiusercontent.com/file-Nz98JJBUj7rzXmJAyALeuV87?se=2123-10-16T04%3A49%3A04Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3D476c2153-1121-4e4c-ad3b-ea164ec21499.png&sig=8TkhKoq6xPQYcrXHiCzvp2SV0G9k3jMllTAy3fe30R8%3D",
+  },
+];
